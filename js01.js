fe04bd05b6f78b901632c1dda59d4c536d04381a
infq23r4 <finqui7@gmail.com>
2022-05-08 01:33:42 -0300

    Object literal

--- js01.js
+++ js01.js
@@ -1,6 +1,6 @@
 //primitive data
 
-console.log(typeof "hello word")
+console.log(typeof "hello word".toUpperCase)
 console.log(typeof 100)
 console.log(typeof false)
 
@@ -14,3 +14,28 @@ console.log(typeof new Number(100))
 console.log(typeof new String("hello word"))
 console.log(typeof new Boolean(true))
 
+console.log( new Number(100))
+console.log( new String("hello word"))
+console.log( new Boolean(true))
+
+
+// Object Literal
+
+console.log( {} )
+
+let name = "Rayn"
+let lastname = "Ray"
+let age = 30
+
+const user = {
+    name: "Rayn",
+    lastName: "Ray",
+    age :30,
+    hobbies: ["read", "progaming", "run"],
+    address: {
+        street: "some street",
+        city: "londdon"
+    }
+}
+
+console.log(user)
