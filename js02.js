c7095cd54c239779f82a9a9350a84a055cea57a3
infq23r4 <finqui7@gmail.com>
2022-05-08 02:16:12 -0300

    This (javaScript OOP)

--- js02.js
+++ js02.js
@@ -1,3 +1,32 @@
-let theNumber = Number(prompt("Pick a number"));
-console.log("Your number is the square root of " +
-theNumber * theNumber);
+//this (javaScript OOP)
+
+const user = {
+    name: "joe", //properties
+    lastname: "Ted",
+    age: 30,
+    showFullName(){
+        return this.name + " " + this.lastname
+    }
+}
+console.log(user)
+console.log(user.showFullName())
+
+const account = {
+    number: "74323133554565654",
+    amount: 100,
+    deposit(quantity){
+        this.amount = this.amount + quantity
+        console.log(this.amount)
+    },
+    withdraw(quantity) {
+        this.amount = this.amount - quantity
+    }
+}
+account.deposit(100)
+account.deposit(50)
+account.deposit(10)
+console.log(account)
+account.withdraw(10)
+account.withdraw(200)
+
+console.log(account.amount)
