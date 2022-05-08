2b99df28c8ae7108b9a59e6eb62f158e3623f80c
infq23r4 <finqui7@gmail.com>
2022-05-08 01:19:07 -0300

    prim-data

---
+++ js01.js
@@ -0,0 +1,16 @@
+//primitive data
+
+console.log(typeof "hello word")
+console.log(typeof 100)
+console.log(typeof false)
+
+console.log(typeof {})
+console.log(typeof [])
+
+console.log(typeof new Date())
+console.log(typeof new RegExp())
+
+console.log(typeof new Number(100))
+console.log(typeof new String("hello word"))
+console.log(typeof new Boolean(true))
+
